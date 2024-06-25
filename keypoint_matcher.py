f0e3f72f947ed2900e18708ef6d000c749e14fc9
Peter Lototsky <40444566+peterlototsky@users.noreply.github.com>
2024-06-25 15:51:44 -0700

    Refactoring Code

--- keypoint_matcher.py
+++ keypoint_matcher.py
@@ -14,27 +14,39 @@ TOP_MATCHES = 20
 def find(image : np.array):
     return SIFT.detectAndCompute(image, None)
 
+
 def homography(image : np.array):
     h, w = image.shape[:2]
     return cv2.warpPerspective(image, H, (int(w * H[0, 0] + w * H[0, 1]), int(h * H[1, 1] + h * H[1, 0])))
+
+def homography_custom(image : np.array, H_custom):
+    h, w = image.shape[:2]
+    return image
+
+
+def calculate_homography(key_points_1, key_points_2, valid_points):
+    key_points_1 = np.float32([key_points_1[i].pt for (_, i) in valid_points])
+    key_points_2 = np.float32([key_points_2[i].pt for (i, _) in valid_points])
+    H, _ = cv2.findHomography(key_points_1, key_points_2, cv2.RANSAC,5.0)
+    return H
 
 
 def match(descriptors_1, descriptors_2):
     matcher = cv2.BFMatcher(normType=cv2.NORM_L2)
     matches = matcher.knnMatch(descriptors_1, descriptors_2,k=2)
-    
+
     valid_points = []
     valid_matches = []
     for match_1, match_2 in matches:
         if match_1.distance < RATIO * match_2.distance:
             valid_points.append((match_1.trainIdx, match_1.queryIdx))
             valid_matches.append([match_1])
-    valid_matches = sorted(matches, key=lambda x: x[0].distance)
-    return valid_points, valid_matches[:TOP_MATCHES]
+    return valid_points, valid_matches
             
             
 def output_mapping(image_1 : np.array, keypoint_1, image_2 : np.array, keypoint_2, valid_matches):
     return cv2.drawMatchesKnn(image_1, keypoint_1, image_2, keypoint_2, valid_matches, None, flags=2)
+
 
 def calculate_accuracy(keypoints_1, keypoints_2, valid_matches):
     keypoints_1_coords = np.array([keypoints_1[match[0].queryIdx].pt for match in valid_matches], dtype=np.float32).reshape(-1, 1, 2)
@@ -47,3 +59,46 @@ def calculate_accuracy(keypoints_1, keypoints_2, valid_matches):
 
     return valid_points_percentage
     
+def blending(image_1, image_2, homography, orientation='left'):
+
+    if orientation == 'left':
+        h, w = image_1.shape[:2]
+        corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+        transformed_corners = cv2.perspectiveTransform(corners.reshape(1, -1, 2), homography).reshape(-1, 2)
+        min_x, min_y = np.min(transformed_corners, axis=0)
+        max_x, max_y = np.max(transformed_corners, axis=0)
+        offset_x = abs(min_x) if min_x < 0 else -1*min_x
+        offset_y = abs(min_y) if min_y < 0 else -1*min_y
+        offset_mat = np.array([[1, 0, offset_x], [0, 1,offset_y], [0,0,1]])
+
+        h, w = image_2.shape[:2]
+        output_width = int(offset_x) + w
+        output_height = max(int(max_y - min_y),h)
+        offset_x_right = int(offset_x if offset_x > 0 else 0)
+        offset_y_right = int(offset_y if offset_y > 0 else 0)
+        
+        canvas = np.zeros((output_height, output_width,3))
+        canvas[offset_y_right:(offset_y_right+h), offset_x_right:(offset_x_right+w), :] = image_2
+        homography = np.dot(offset_mat,homography)
+        left_element = cv2.warpPerspective(image_1, homography, (output_width, output_height))
+        left_element[:,offset_x_right:,:] = 0
+        final_result = left_element + canvas
+    elif orientation == 'right':
+        h, w = image_1.shape[:2]
+        corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+        transformed_corners = cv2.perspectiveTransform(corners.reshape(1, -1, 2), homography).reshape(-1, 2)
+        min_x, min_y = np.min(transformed_corners, axis=0)
+        max_x, max_y = np.max(transformed_corners, axis=0)
+        offset_x = abs(min_x) if min_x < 0 else -1*min_x
+        offset_y = abs(min_y) if min_y < 0 else -1*min_y
+        h, w = image_2.shape[:2]
+        output_width = abs(int(offset_x)) + int(max_x-min_x)
+        output_height = max(int(max_y - min_y),h)
+        canvas = np.zeros((output_height, output_width,3))
+        canvas[0:(h), 0:(w), :] = image_2
+        right_element = cv2.warpPerspective(image_1, homography, (output_width, output_height))
+        right_element[:,:(w),:] = 0
+        final_result = right_element + canvas
+    else:
+        final_result = 0
+    return final_result
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import cv2
 import numpy as np
 import keypoint_matcher as _kp_match
+import os
+
+# Sample Inputs
+Sample_1 = 'IMG_8833.JPG'
+Sample_2 = 'IMG_8834.JPG'
+Sample_3 = 'IMG_8835.JPG'
+
+# Program Paths
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sample_input_dir = 'Sample_Input'
+sample_output_dir = 'Sample_Output'
+
+pano_left_path = 'pano_left.jpg'
+pano_full_path = 'full_pano.jpg'
 
 def display_image(title : str, image : np.array):
     cv2.imshow(title, image)
@@ -8,10 +22,13 @@ def display_image(title : str, image : np.array):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    image_1 = cv2.imread('IMG_8833.JPG')
-    image_2 = cv2.imread('IMG_8834.JPG')
-    image_3 = cv2.imread('IMG_8835.JPG')
-    
+    path = os.path.join(current_dir, sample_input_dir, Sample_1)
+    print(path)
+    image_1 = cv2.imread(os.path.join(current_dir, sample_input_dir, Sample_1))
+    image_2 = cv2.imread(os.path.join(current_dir, sample_input_dir, Sample_2))
+    image_3 = cv2.imread(os.path.join(current_dir, sample_input_dir, Sample_3))
+
+    ''' Matching Demonstrations
     print('Part2a')
     warped_image_1 = _kp_match.homography(image_1)
     display_image('Transformed Image', warped_image_1)
@@ -26,4 +43,40 @@ if __name__ == '__main__':
     print('part2c')
     accuracy = _kp_match.calculate_accuracy(keypoint_1, keypoint_2, valid_matches)
     print("Accuracy: " + str(accuracy))
+    '''
 
+    # Calculate H for image 1 -> image 2
+    display_image('Image 1', image_1)
+    display_image('Image 2', image_2)
+    display_image('Image 3', image_3)
+    print('Question2')
+    keypoint_1, descriptor_1 = _kp_match.find(image_1)
+    keypoint_2, descriptor_2 = _kp_match.find(image_2)
+    valid_points, valid_matches = _kp_match.match(descriptor_1, descriptor_2)
+    H_1 = _kp_match.calculate_homography(keypoint_1, keypoint_2, valid_points)
+    print(H_1)
+    # warped_image_1 = _kp_match.homography_custom(image_1, H_1)
+    # display_image('image1_image2', warped_image_1)
+    # Blend image 1 and 2
+    pano_left = _kp_match.blending(image_1, image_2, H_1)
+    cv2.imwrite(os.path.join(current_dir, sample_output_dir, pano_left_path), pano_left)
+    # Calculate H for image 3 -> image 2 / image 1
+    pano_left = cv2.imread(os.path.join(current_dir, sample_output_dir, pano_left_path)) # reload pano_left
+    display_image('pano_left', pano_left)
+    keypoint_3, descriptor_3 = _kp_match.find(image_3)
+    keypoint_4, descriptor_4 = _kp_match.find(pano_left)
+    valid_points, valid_matches = _kp_match.match(descriptor_3, descriptor_4)
+    H_2 = _kp_match.calculate_homography(keypoint_3, keypoint_4, valid_points)
+    print(H_2)
+    # warped_image_2 = _kp_match.homography_custom(image_3, H_2)
+    # display_image('image1_image3', warped_image_2)
+    # Blend image 4 and 2 / 1
+    pano_full = _kp_match.blending(image_3, pano_left, H_2, 'right')
+    cv2.imwrite(os.path.join(current_dir, sample_output_dir, pano_full_path), pano_full)
+    pano_full = cv2.imread(os.path.join(current_dir, sample_output_dir, pano_full_path))
+    display_image('full_pano', pano_full)
+    
+    if os.path.exists(os.path.join(current_dir, sample_output_dir, pano_left_path)):
+        # Delete the file
+        os.remove(os.path.join(current_dir, sample_output_dir, pano_left_path))
+        print(f"\n\rCleaned up Files..")
